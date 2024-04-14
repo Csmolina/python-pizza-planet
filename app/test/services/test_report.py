9b2efd9ab39196e4711a45acfc1044ffda6498b3
Cristopher Molina <58301825+Csmolina@users.noreply.github.com>
2024-04-13 23:58:38 -0500

    refactor: change asserts to pytest assume in tests, also delete unused imports

--- app/test/services/test_report.py
+++ app/test/services/test_report.py
@@ -3,7 +3,7 @@ import pytest
 def test_get_report_service(client, report_uri, create_order):
   response = client.get(report_uri)
   report_data = response.json
-  assert response.status.startswith("200")
-  assert report_data["top_ingredient"]
-  assert report_data["top_month"]
-  assert report_data["top_three_clients"]
+  pytest.assume(response.status.startswith("200")) 
+  pytest.assume(report_data["top_ingredient"])
+  pytest.assume(report_data["top_month"])
+  pytest.assume(report_data["top_three_clients"])
